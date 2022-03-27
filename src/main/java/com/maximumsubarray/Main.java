1f6ed3eca73ba10f7f687e81eab007df0a336d7f
aliaksei Beliak <alexbeljak99@gmail.com>
2022-03-27 11:49:53 +0300

    added max-crossing-subarray nlogn implementation

---
+++ src/main/java/com/maximumsubarray/Main.java
@@ -0,0 +1,66 @@
+package com.maximumsubarray;
+
+import java.util.Arrays;
+
+public class Main {
+
+    public static int[] findMaxCrossingSubarray(int[] array, int low, int middle, int high) {
+        int leftSum = Integer.MIN_VALUE, rightSum = Integer.MIN_VALUE;
+        int maxLeftIndex = 0, maxRightIndex = 0;
+        int sum = 0;
+
+        for (int i = middle; i >= low; i--) {
+            sum += array[i];
+            if (sum > leftSum) {
+                leftSum = sum;
+                maxLeftIndex = i;
+            }
+        }
+
+        sum = 0;
+
+        for (int i = middle + 1; i <= high; i++) {
+            sum += array[i];
+            if (sum > rightSum) {
+                rightSum = sum;
+                maxRightIndex = i;
+            }
+        }
+
+        return new int[]{maxLeftIndex, maxRightIndex, leftSum + rightSum};
+    }
+
+    public static int[] findMaximumSubarray(int[] array, int low, int high) {
+        int lowSubarrayTupleIndex = 0;
+        int highSubarrayTupleIndex = 1;
+        int sumTupleIndex = 2;
+
+        if (low == high) {
+            return new int[]{low, high, array[low]};
+        }
+
+        int middle = (low + high) / 2;
+
+        int[] leftSubarrayTuple = findMaximumSubarray(array, low, middle);
+        int[] rightSubarrayTuple = findMaximumSubarray(array, middle + 1, high);
+        int[] crossSubarrayTuple = findMaxCrossingSubarray(array, low, middle, high);
+
+        if (leftSubarrayTuple[sumTupleIndex] >= rightSubarrayTuple[sumTupleIndex] &&
+                leftSubarrayTuple[sumTupleIndex] >= crossSubarrayTuple[sumTupleIndex]) {
+            return new int[]{leftSubarrayTuple[lowSubarrayTupleIndex], leftSubarrayTuple[highSubarrayTupleIndex],
+                    leftSubarrayTuple[sumTupleIndex]};
+        } else if (rightSubarrayTuple[sumTupleIndex] >= leftSubarrayTuple[sumTupleIndex] &&
+                rightSubarrayTuple[sumTupleIndex] >= crossSubarrayTuple[sumTupleIndex]) {
+            return new int[]{rightSubarrayTuple[lowSubarrayTupleIndex], rightSubarrayTuple[highSubarrayTupleIndex],
+                    rightSubarrayTuple[sumTupleIndex]};
+        } else {
+            return new int[]{crossSubarrayTuple[lowSubarrayTupleIndex], crossSubarrayTuple[highSubarrayTupleIndex],
+                    crossSubarrayTuple[sumTupleIndex]};
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] array = new int[]{-2, -3, 4, -1, -2, 1, 5, -3};
+        System.out.println(Arrays.toString(findMaximumSubarray(array, 0, array.length - 1)));
+    }
+}
