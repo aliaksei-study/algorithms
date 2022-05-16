a65237415e3b98d332bcb2ee27b587efef205256
aliaksei Beliak <alexbeljak99@gmail.com>
2022-05-16 23:06:32 +0300

    heap and heapsort implementation

---
+++ src/main/java/com/sorting/heapsort/Main.java
@@ -0,0 +1,25 @@
+package com.sorting.heapsort;
+
+import com.heap.Heap;
+
+import java.util.Arrays;
+
+public class Main {
+
+    public static void heapsort(int[] array) {
+        Heap heap = new Heap(array);
+        heap.buildMaxHeap();
+
+        for(int i = array.length - 1; i > 0; i--) {
+            heap.swap(0, i);
+            heap.setMaxHeapSize(heap.getMaxHeapSize() - 1);
+            heap.maxHeapify(i);
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] testArray = new int[]{15, 8, 2};
+        heapsort(testArray);
+        System.out.println(Arrays.toString(Arrays.stream(testArray).toArray()));
+    }
+}
