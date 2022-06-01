acbe86b66a53e73e8a9b8e169ff57bafd538dd9b
aliaksei Beliak <alexbeljak99@gmail.com>
2022-06-01 22:26:24 +0300

    [feat] quicksort implementation

---
+++ src/main/java/com/sorting/quicksort/Main.java
@@ -0,0 +1,40 @@
+package com.sorting.quicksort;
+
+import java.util.Arrays;
+
+public class Main {
+
+    public static int partition(int[] array, int left, int right) {
+        int lastElement = array[right];
+        int i = left - 1;
+
+        for (int j = left; j < right; j++) {
+            if (array[j] <= lastElement) {
+                i++;
+                swap(array, i, j);
+            }
+        }
+        swap(array, i + 1, right);
+        return i + 1;
+    }
+
+    public static void swap(int[] array, int sourceIndex, int destinationIndex) {
+        int temp = array[sourceIndex];
+        array[sourceIndex] = array[destinationIndex];
+        array[destinationIndex] = temp;
+    }
+
+    public static void quicksort(int[] array, int left, int right) {
+        if (left < right) {
+            int partitionIndex = partition(array, left, right);
+            quicksort(array, left, partitionIndex - 1);
+            quicksort(array, partitionIndex + 1, right);
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] array = new int[]{13, 19, 9, 5, 12, 8, 7, 4, 21, 26, 11};
+        quicksort(array, 0, array.length - 1);
+        System.out.println(Arrays.toString(array));
+    }
+}
